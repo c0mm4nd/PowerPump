762c1e53f3b6669db6acb050cd8b1d61164bea1d
Command M <Command M>
2016-05-31 12:38:15 +0800

    Signed-off-by: Command M <Command M>

--- platforms/android/assets/www/js/map.js
+++ platforms/android/assets/www/js/map.js
@@ -1,5 +1,4 @@
 // Map
-var map = new BMap.Map("allmap");
 var geolocation = new BMap.Geolocation();
 var myPosition,myPoint;
 geolocation.getCurrentPosition(function(r){
@@ -12,24 +11,26 @@ geolocation.getCurrentPosition(function(r){
 },{enableHighAccuracy: true});
 var radius = 100000;
 
-$(document).on("pageInit", "#map", function(e, pageId, $page) {
+$(document).on("pageInit", "#map", function(e, pageId, page) {
+var nearmap = new BMap.Map("nearmap");
+nearmap.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
   var myPoint = myPosition.point;
   var mk = new BMap.Marker(myPoint);
-  var local = new BMap.LocalSearch(map, {
-    renderOptions:{map: map}
+  var local = new BMap.LocalSearch(nearmap, {
+    renderOptions:{map: nearmap}
   });
-  map.addOverlay(mk);
+  nearmap.addOverlay(mk);
   mk.setAnimation( BMAP_ANIMATION_BOUNCE );
   // 百度地图API功能
-  map.centerAndZoom(myPoint,16);
-    local.searchNearby("电", myPoint, radius,{customData:{geotableId:140069}});
+  nearmap.centerAndZoom(myPoint,16);
+  local.searchNearby("电", myPoint, radius,{customData:{geotableId:140069}});
   
-    map.addEventListener("click",function(e){
-    $('#allmap').height('50%');
+  nearmap.addEventListener("click",function(e){
+    $('#nearmap').height('50%');
     var p1 = myPoint;
     var p2 = e;
-    var driving = new BMap.DrivingRoute(map, {renderOptions:{map: map,  panel: "r-result", autoViewport: true}});
-    map.clearOverlays;
+    var driving = new BMap.DrivingRoute(nearmap, {renderOptions:{map: nearmap,  panel: "r-result", autoViewport: true}});
+    nearmap.clearOverlays;
     driving.search(p1, p2);
 
   });
@@ -38,12 +39,12 @@ $(document).on("pageInit", "#map", function(e, pageId, $page) {
   /*清理标注并重新初始化定位*/
   function relocate(){
     $.router.load('#map', true);
-    map.clearOverlays();
+    nearmap.clearOverlays();
     geolocation.getCurrentPosition(function(r){
       if(this.getStatus() == BMAP_STATUS_SUCCESS){
         var mk = new BMap.Marker(myPoint);
-        map.panTo(myPoint);
-        map.addOverlay(mk);
+        nearmap.panTo(myPoint);
+        nearmap.addOverlay(mk);
         mk.setAnimation( BMAP_ANIMATION_BOUNCE ); 
         myPosition = r;     
         // alert('您的位置:'+myPoint.lng+','+myPoint.lat);
--- platforms/android/assets/www/js/plan.js
+++ platforms/android/assets/www/js/plan.js
@@ -1,4 +1,5 @@
-// Map
+// planmap
+var planmap = new BMap.Map("planmap");
 var geolocation = new BMap.Geolocation();
 var myPosition,myPoint;
 geolocation.getCurrentPosition(function(r){
@@ -11,26 +12,24 @@ geolocation.getCurrentPosition(function(r){
 },{enableHighAccuracy: true});
 var radius = 100000;
 
-$(document).on("pageInit", "#map", function(e, pageId, page) {
-var nearmap = new BMap.Map("nearmap");
-nearmap.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
+$(document).on("pageInit", "#plan", function(e, pageId, $page) {
   var myPoint = myPosition.point;
   var mk = new BMap.Marker(myPoint);
-  var local = new BMap.LocalSearch(nearmap, {
-    renderOptions:{map: nearmap}
+  var local = new BMap.LocalSearch(planmap, {
+    renderOptions:{map: planmap}
   });
-  nearmap.addOverlay(mk);
+  planmap.addOverlay(mk);
   mk.setAnimation( BMAP_ANIMATION_BOUNCE );
   // 百度地图API功能
-  nearmap.centerAndZoom(myPoint,16);
-  local.searchNearby("电", myPoint, radius,{customData:{geotableId:140069}});
+  planmap.centerAndZoom(myPoint,16);
+    local.searchNearby("电", myPoint, radius,{customData:{geotableId:140069}});
   
-  nearmap.addEventListener("click",function(e){
-    $('#nearmap').height('50%');
+    planmap.addEventListener("click",function(e){
+    $('#allmap').height('50%');
     var p1 = myPoint;
     var p2 = e;
-    var driving = new BMap.DrivingRoute(nearmap, {renderOptions:{map: nearmap,  panel: "r-result", autoViewport: true}});
-    nearmap.clearOverlays;
+    var driving = new BMap.DrivingRoute(planmap, {renderOptions:{map: planmap,  panel: "r-result", autoViewport: true}});
+    planmap.clearOverlays;
     driving.search(p1, p2);
 
   });
@@ -38,13 +37,13 @@ nearmap.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
 
   /*清理标注并重新初始化定位*/
   function relocate(){
-    $.router.load('#map', true);
-    nearmap.clearOverlays();
+    $.router.load('#plan', true);
+    planmap.clearOverlays();
     geolocation.getCurrentPosition(function(r){
       if(this.getStatus() == BMAP_STATUS_SUCCESS){
         var mk = new BMap.Marker(myPoint);
-        nearmap.panTo(myPoint);
-        nearmap.addOverlay(mk);
+        planmap.panTo(myPoint);
+        planmap.addOverlay(mk);
         mk.setAnimation( BMAP_ANIMATION_BOUNCE ); 
         myPosition = r;     
         // alert('您的位置:'+myPoint.lng+','+myPoint.lat);
